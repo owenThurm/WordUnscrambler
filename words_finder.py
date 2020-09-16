3d22f87eaefe87343b0b64fd7e01c786ad6cbd6c
owenThurm <owen.p.thurm@gmail.com>
2020-09-15 22:06:55 -0400

    Added the english checking functionality

--- words_finder.py
+++ words_finder.py
@@ -1,2 +1,31 @@
-from nltk import words
+"""
+a python script to check which words in a file are english words
+"""
+import ssl
+import nltk
 
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+
+from nltk.corpus import words
+
+print('mead' in words.words())
+
+english_words = []
+i=1
+with open('/Users/othurm/desktop/repos/WordUnscrambler/Strings.txt', 'r') as permutations:
+    for line in permutations:
+        print(line)
+        for word in line.split("]["):
+            print(word)
+            print(i)
+            i = i+1
+            if(word in words.words()):
+                english_words.append(word)
+
+print(english_words)
