4a64b8e40223344e04d14484fa25704066177960
owenThurm <owen.p.thurm@gmail.com>
2020-09-22 17:59:28 -0400

    added file writing

--- StringPermutations.java
+++ StringPermutations.java
@@ -1,15 +1,23 @@
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.File;
+import java.io.FileWriter;
+
+
 class Solution {
-    public List<List<Character>> permute(char[] characters) {
-        if(nums.length == 0) return new ArrayList(Arrays.asList(new ArrayList()));
-        if(nums.length == 1) return new ArrayList(Arrays.asList(new ArrayList(Arrays.asList(nums[0]))));
-        if(nums.length == 2) return new ArrayList(Arrays.asList(new ArrayList(Arrays.asList(nums[0], nums[1])), new ArrayList(Arrays.asList(nums[1], nums[0]))));
+    public static List<List<Character>> permute(char[] characters) {
+        if(characters.length == 0) return new ArrayList(Arrays.asList(new ArrayList()));
+        if(characters.length == 1) return new ArrayList(Arrays.asList(new ArrayList(Arrays.asList(characters[0]))));
+        if(characters.length == 2) return new ArrayList(Arrays.asList(new ArrayList(Arrays.asList(characters[0], characters[1])), new ArrayList(Arrays.asList(characters[1], characters[0]))));
         List<List<Character>> permutations = new ArrayList();
         for(Character character : characters) {
             //get all of the permutations of the rest of the list and add this num to the beginning
-            List<List<Character>> restPerms = permute(this.remove(characters, character));
-            for(int i=0; i<this.remove(characters, character).length; i++) System.out.print(this.remove(characters, character)[i]);
+            List<List<Character>> restPerms = permute(remove(characters, character));
+            for(int i=0; i< remove(characters, character).length; i++) System.out.print(remove(characters, character)[i]);
             System.out.println();
-            for(List<Integer> perm : restPerms) {
+            for(List<Character> perm : restPerms) {
                 perm.add(0, character);
             }
             //add those permutations to the list of permutations
@@ -18,14 +26,15 @@ class Solution {
         return permutations;
     }
     
-    public char[] remove(char[] nums, char num) {
+    public static char[] remove(char[] nums, char num) {
         char[] removed = new char[nums.length-1];
         int j = 0;
+        boolean removedAlready = false;
         for(int i=0; i<nums.length; i++) {
-            if(nums[i] != num) {
+            if(removedAlready || nums[i] != num) {
                 removed[j] = nums[i];
                 j++;
-            }
+            } else removedAlready = true;
         }
         return removed;
     }
@@ -33,7 +42,25 @@ class Solution {
 
     public static void main(String[] args) {
 
-        System.out.prinln(permute(new char[] {a, b, c}));
+        System.out.println(Boolean.parseBoolean("FaLSe"));
+/*
+         try {
+             File permsFile = new File("Strings.txt");
+             FileWriter writer = new FileWriter(permsFile, true);
+             String permutationString = permute(new char[] { 'm', 'i', 'l', 'u', 'e', 's', 's'}).toString().replaceAll(", ", "").replaceAll("]", " ").replaceAll("\\[", "");
+
+             System.out.println(permutationString.substring(0, 4));
+             writer.write(permutationString);
+             writer.close();
+         } catch(IOException e) {
+             throw new IllegalStateException("IO Exception!", e);
+         }
+
+
+*/
+
+
+
 
     }
 }
